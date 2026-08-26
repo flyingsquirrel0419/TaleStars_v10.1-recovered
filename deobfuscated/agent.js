51bf92e75c9bdab172ffcc2ea711854dacd874f2
flyingsquirrel <flyingsquirrel@example.com>
2026-08-26 15:38:01 +0000

    Add bundle files batch 001

---
+++ deobfuscated/agent.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Readable equivalent of the observed execution path in Tale.framework/build.js.
+function runRecoveredAgent() {
+  console.log("THE BLACKWHALE");
+}
+
+runRecoveredAgent();
